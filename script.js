a700625ab2145b1c34b822fc8bd4572ff7b1531b
luischo04 <buenrostroluisfernando04@gmail.com>
2021-12-15 22:39:50 -0600

    scripts actualizados
    Aplicado opcion para instalar

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./sw.js')
-    .then(reg => console.log('Registro de SW exitoso', reg))
-    .catch(err => console.warn('Error al tratar de registrar el sw', err))
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("sw.js")
+      .then(res => console.log("service worker registrado"))
+      .catch(err => console.log("service worker no registrado", err));
+  });
 }
